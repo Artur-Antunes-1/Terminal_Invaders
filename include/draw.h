a5ca34b2e773aa84ab70e59ed493c1153f0764b8
arturantunes <aas4@cesar.school>
2025-05-20 14:50:48 -0300

    adiciona elementos iniciais

--- include/draw.h
+++
@@ -1,7 +0,0 @@
-#ifndef DRAW_H
-#define DRAW_H
-
-// desenha tudo na tela
-void draw_frame(void);
-
-#endif // DRAW_H
--- include/input.h
+++
@@ -1,7 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-// inicializa controles e lê tecla
-void process_input(int *running);
-
-#endif // INPUT_H
